4f2081997bc4451a543d4aa5c477a3e4684673b9
kristian334 <kdzidz00@fesb.hr>
2022-05-31 16:22:44 +0200

    Add project files.

---
+++ MarinoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarinoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
